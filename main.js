b315c2fe82d23f549d66c8920746969f36d5f6f9
nicogalvang <nico.galvangarcia@gmail.com>
2024-06-18 12:58:21 +0200

    /suma

--- main.js
+++ main.js
@@ -2,12 +2,14 @@ const express = require('express')
 const app = express()
 const port = 3000 
 
-app.get('/hola', (req, res) => {
-  res.send('Hello world')
+app.get('/suma', (req, res) => {
+    var sumando1 = 6
+    var sumando2 = 10
+    var resultado = sumando1 + sumando2
+  res.send("El resultado es " + resultado)
 })
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
 
-app.use(express.static(`24-prueba-uf1287`))
