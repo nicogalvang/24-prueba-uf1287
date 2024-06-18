bffaa862c4a5e72d53e41c10b4d455ed32591050
nicogalvang <nico.galvangarcia@gmail.com>
2024-06-18 11:28:36 +0200

    /hola en la URL

--- main.js
+++ main.js
@@ -1,11 +1,13 @@
 const express = require('express')
 const app = express()
-const port = 3000
+const port = 3000 
 
-app.get('/', (req, res) => {
+app.get('/hola', (req, res) => {
   res.send('Hello world')
 })
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
+
+app.use(express.static(`24-prueba-uf1287`))
